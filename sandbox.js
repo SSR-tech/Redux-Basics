97cd53b77675ac906d0ad1ba2751f6042447c4e8
SSR <ssrstudio99@gmail.com>
2020-09-10 17:33:50 +0530

    01 - JS App and Action created

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,10 @@
-console.log("REDUX");
+// String constant indecating the type of action.
+const BUY_CAKE = "BUY_CAKE";
+
+// Action Creator
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "First Redux Action",
+  };
+}
