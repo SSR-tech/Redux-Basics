d253d42e4577adb2306d1c1d48e351c1b308c7f2
SSR <ssrstudio99@gmail.com>
2020-09-10 17:42:29 +0530

    02 - Reducers

--- sandbox.js
+++ sandbox.js
@@ -8,3 +8,25 @@ function buyCake() {
     info: "First Redux Action",
   };
 }
+
+// Reducers - Specify how the app's state changes in response to actions sent to the store.
+
+// Function that accepts state and action as arguements, and returns the next state of the application.
+
+// (previousState, action) => newState
+
+const initialState = {
+  numberOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state, // Creating a copy of the state. This won't affect the other properties if any.
+        numOfCakes: state.numberOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
